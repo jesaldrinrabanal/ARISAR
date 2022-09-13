6b123744144bc2be8c03034f6d2698a2ac4a738d
patqnts <jayquintos56@gmail.com>
2022-09-13 15:46:13 +0800

    1st challenge progress

---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Animator anim;
+    public float speed = 6f;
+    public float turnsmooth = 0.1f;
+    float turnSmoothVelocity;
+
+    Vector3 velocity;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            Run();
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnsmooth);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+      
+    }
+    private void Run()
+    {
+        anim.SetBool("Run", true);
+    }
+}
