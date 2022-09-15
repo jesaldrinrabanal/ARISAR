c38e2b4388957125a48a30dd7980ae9b7efa92e7
patqnts <jayquintos56@gmail.com>
2022-09-15 22:00:02 +0800

    Reload Scene - Fix scene

--- Assets/SelectStorybook.cs
+++ Assets/SelectStorybook.cs
@@ -13,4 +13,8 @@ public class SelectStorybook : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
