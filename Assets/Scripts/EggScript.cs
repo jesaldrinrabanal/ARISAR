3c6948244e0f0bbfda569c0d4958f97050d2d8bf
ARISmobileapp <mobileapp.aris@gmail.com>
2022-09-16 18:24:30 +0800

    page5

--- Assets/Scripts/EggScript.cs
+++ Assets/Scripts/EggScript.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class EggScript : MonoBehaviour
 {
-    public int eggCount;
-   public void Update()
+    public GameObject egg1;
+    public GameObject egg2;
+    public GameObject egg3;
+    public GameObject egg4;
+    public GameObject egg5;
+    public GameObject destroyObject;
+    public GameObject completed;
+    public int eggCount = 0;
+
+    public void Update()
     {
         if (eggCount == 5)
         {
             Debug.Log("Complete");
+            destroy();
+            completed.gameObject.SetActive(true);
         }
+    }
+    public void destroy()
+    {
+        Destroy(destroyObject.gameObject);
     }
     public void eggFound()
     {
+        Debug.Log("Egg Found");
         eggCount++;
+    }
+    public void Egg1()
+    {
+        egg1.gameObject.SetActive(false);
+    }
+    public void Egg2()
+    {
+        egg2.gameObject.SetActive(false);
+    }   
+    public void Egg3()
+    {
+        egg3.gameObject.SetActive(false);
+    }
+    public void Egg4()
+    {
+        egg4.gameObject.SetActive(false);
+    }
+    public void Egg5()
+    {
+        egg5.gameObject.SetActive(false);
     }
 }
