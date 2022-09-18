cb9c535e788d82a4c2ccc2b18d1c97c6cfd6eb4c
patqnts <jayquintos56@gmail.com>
2022-09-18 23:50:34 +0800

    18-9finalupdate

--- Assets/3Scripts/Lastpage.cs
+++ Assets/3Scripts/Lastpage.cs
@@ -6,15 +6,17 @@ public class Lastpage : MonoBehaviour
 {
     public GameObject complete;
     public GameObject wrong;
-
+  
     public void Correct() {
         complete.gameObject.SetActive(true);
         Destroy(wrong.gameObject);
         Debug.Log("Correct!");
+        
     }
     public void Wrong() {
         wrong.gameObject.SetActive(true);
         Debug.Log("Wrong!");
 
     }
+ 
 }
