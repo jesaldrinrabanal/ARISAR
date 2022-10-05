da5b1fa3c9106bba73c89e2d579adc6140fb6e77
patqnts <jayquintos56@gmail.com>
2022-10-05 15:45:37 +0800

    page-3

---
+++ Assets/old_answer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class old_answer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject correct;
+    public GameObject finish;
+    public GameObject Bluex;
+    public GameObject Sapphirex;
+    public GameObject Purpx;
+    public GameObject goldx;
+    public void Gold()
+    {
+        Debug.Log("GOLD");
+        finish.gameObject.SetActive(true);
+        correct.gameObject.SetActive(false);
+        goldx.gameObject.SetActive(true);
+    }
+    public void Blue()
+    {
+        Debug.Log("Blue");
+        Bluex.gameObject.SetActive(true);
+    }
+    public void Sapphire()
+    {
+        Debug.Log("Sapphire");
+        Sapphirex.gameObject.SetActive(true);
+    }
+    public void Purp()
+    {
+        Debug.Log("Purp");
+        Purpx.gameObject.SetActive(true);
+    }
+}
