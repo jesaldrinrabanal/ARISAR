5006197140f6e0d83ac1e02dd7d07f260d7bc8cf
patqnts <jayquintos56@gmail.com>
2022-12-11 21:31:26 +0800

    fix

--- Assets/3Scripts/BGM.cs
+++ Assets/3Scripts/BGM.cs
@@ -21,7 +21,7 @@ public class BGM : MonoBehaviour
     private void Update()
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        if (currentScene.name == "Manok 2"|| currentScene.name == "English" || currentScene.name == "Merge" || currentScene.name == "Old-English")
+        if (currentScene.name == "ManokTag"|| currentScene.name == "Manok" || currentScene.name == "OldTag" || currentScene.name == "Old-English")
         {
             // Stops playing music in level 1 scene
             Destroy(gameObject);
